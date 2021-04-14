2e49fdd24a004601886161bd52e69da2fca03849
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-14 16:28:23 +0200

    Computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,26 @@
 <template>
   <div class="home">
     <h1>arek blog</h1>
-    <h2>refs</h2>
-    <p>{{ userOne.name }} - {{ userOne.age }}</p>
-    <button @click="updateUserOne">Update user one</button>
-    <h2>reactive</h2>
-    <p>{{ userTwo.name }} - {{ userTwo.age }}</p>
-    <button @click="updateUserTwo">Update user two</button>
+    <input type="text" v-model="search" />
+    <p>search term: {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
+import { computed, ref } from "vue";
 export default {
   name: "Home",
   setup() {
-    // const par = ref(null);
+    const search = ref("");
+    const names = ref(["arek", "czarek", "marek", "darek", "jarek"]);
 
-    const userOne = ref({ name: "Arek", age: "35" });
-    const userTwo = reactive({ name: "Marek", age: "25" });
-
-    // ref are better ;)
-
-    const updateUserOne = () => {
-      userOne.value.age = 36;
-    };
-    const updateUserTwo = () => {
-      userTwo.age = 28;
-    };
-
-    return { userOne, updateUserOne, userTwo, updateUserTwo };
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+    return { names, search, matchingNames };
   },
 };
 </script>
