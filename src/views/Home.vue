3662489ae6386c532d713c79df317ac43d7a0a86
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-14 16:01:22 +0200

    Template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="home">
     <h1>arek blog</h1>
-    <p>My name is {{ name }} and my age is {{ age }}.</p>
+    <p ref="par">My name is {{ name }} and my age is {{ age }}.</p>
     <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   name: "Home",
   setup() {
     console.log("setup");
 
+    const par = ref(null);
+    console.log(par, par.value);
+
     let name = "Arek";
     let age = 35;
 
     const handleClick = () => {
-      console.log("clicked");
+      console.log(par, par.value);
+      par.value.classList.add("test");
+      par.value.textContent = "hello world";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, par };
   },
 };
 </script>
