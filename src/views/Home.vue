53678c42d78fbfeee622f7e121e24a791f987dc8
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-14 18:53:51 +0200

    Fetch data in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <h1>arek blog</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="handleClick">toggle post</button>
-    <button @click="posts.pop()">delete post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length"><PostList :posts="posts" /></div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
@@ -14,28 +14,25 @@ export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: "Welcome to the blog",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquid modi eos tempore maiores, labore quisquam eum consectetur suscipit hic! Minima sunt aliquid quae assumenda beatae vitae eaque numquam, tempora quas ab ex blanditiis reprehenderit minus nesciunt adipisci excepturi temporibus alias sapiente, quia facilis, necessitatibus dolore. Saepe, provident vitae, non ut est alias excepturi amet perferendis facere vel, nostrum laudantium hic dolorem ullam quasi repudiandae accusamus voluptas consequatur? Incidunt dolore nesciunt repellendus aut maiores maxime voluptatum sunt, rem eius, repudiandae laboriosam quasi totam, deserunt ea facilis similique beatae vero delectus perferendis ullam. Repudiandae non quo, sint laborum fugiat quia consectetur.",
-        id: 1,
-      },
-      {
-        title: "Top 5 css tips",
-        body:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam aliquid modi eos tempore maiores, labore quisquam eum consectetur suscipit hic! Minima sunt aliquid quae assumenda beatae vitae eaque numquam, tempora quas ab ex blanditiis reprehenderit minus nesciunt adipisci excepturi temporibus alias sapiente, quia facilis, necessitatibus dolore. Saepe, provident vitae, non ut est alias excepturi amet perferendis facere vel, nostrum laudantium hic dolorem ullam quasi repudiandae accusamus voluptas consequatur? Incidunt dolore nesciunt repellendus aut maiores maxime voluptatum sunt, rem eius, repudiandae laboriosam quasi totam, deserunt ea facilis similique beatae vero delectus perferendis ullam. Repudiandae non quo, sint laborum fugiat quia consectetur.",
-        id: 2,
-      },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
 
-    const showPosts = ref(true);
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("No data bro...");
+        }
 
-    const handleClick = () => {
-      showPosts.value = !showPosts.value;
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(err.message);
+      }
     };
+    load();
 
-    return { posts, showPosts, handleClick };
+    return { posts, error };
   },
 };
 </script>
