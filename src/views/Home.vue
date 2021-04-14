b7aff14d07d797c626c93bae8f9c91744e8a2f9c
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-14 15:55:38 +0200

    Setup composition api - basics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
     <h1>arek blog</h1>
+    <p>My name is {{ name }} and my age is {{ age }}.</p>
+    <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  setup() {
+    console.log("setup");
+
+    let name = "Arek";
+    let age = 35;
+
+    const handleClick = () => {
+      console.log("clicked");
+    };
+
+    return { name, age, handleClick };
+  },
 };
 </script>
